edb1b81ef1d5586a483c6edd404d2581cb8c6144
Hendri Pretorius <pretorh@gmail.com>
2015-06-14 20:42:01 +0200

    move tests into nested describe

--- test/contextBuilder-tests.js
+++ test/contextBuilder-tests.js
@@ -3,20 +3,22 @@ var assert = require("assert");
 var contextBuilder = require("../lib/contextBuilder");
 
 describe("contextBuilder", function() {
-    it("object fields are the file names", function() {
-        var o = contextBuilder.build(__dirname + "/../sample/context/", ["index.json"]);
-        assert.notStrictEqual(undefined, o.index);
-    });
+    describe("build", function() {
+        it("object fields are the file names", function() {
+            var o = contextBuilder.build(__dirname + "/../sample/context/", ["index.json"]);
+            assert.notStrictEqual(undefined, o.index);
+        });
 
-    it("parse the json file", function() {
-        var o = contextBuilder.build(__dirname + "/../sample/context/", ["index.json"]);
-        assert.equal("John Doe", o.index.name);
-    });
+        it("parse the json file", function() {
+            var o = contextBuilder.build(__dirname + "/../sample/context/", ["index.json"]);
+            assert.equal("John Doe", o.index.name);
+        });
 
-    it("maps files recursively in sub objects", function() {
-        var o = contextBuilder.build(__dirname + "/../sample/context/", ["sub/more.json"]);
-        assert.notStrictEqual(undefined, o.sub);
-        assert.notStrictEqual(undefined, o.sub.more);
-        assert.equal(1, o.sub.more.key);
+        it("maps files recursively in sub objects", function() {
+            var o = contextBuilder.build(__dirname + "/../sample/context/", ["sub/more.json"]);
+            assert.notStrictEqual(undefined, o.sub);
+            assert.notStrictEqual(undefined, o.sub.more);
+            assert.equal(1, o.sub.more.key);
+        });
     });
 });
