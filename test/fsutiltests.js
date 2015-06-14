f0dfe8abe060748363dd40271b1a4aef75ac5f14
Hendri Pretorius <pretorh@gmail.com>
2015-06-14 18:54:52 +0200

    return list of files in fsutils.filelist

--- test/fsutiltests.js
+++ test/fsutiltests.js
@@ -4,5 +4,11 @@ var fsutils = require("../lib/fsutils");
 
 describe("fs utils", function() {
     describe("file list", function() {
+        it("returns array of file names", function() {
+            var files = fsutils.filelist(__dirname + "/../sample/www", "");
+            assert.notStrictEqual(undefined, files);
+            assert.equal("Array", files.constructor.name);
+            assert.notEqual(-1, files.indexOf("index.html"));
+        });
     });
 });
