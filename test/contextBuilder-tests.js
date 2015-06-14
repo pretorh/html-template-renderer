765d52ebcd00bc32aae5ab9f1042a0cab682e540
Hendri Pretorius <pretorh@gmail.com>
2015-06-14 19:36:44 +0200

    contextBuilder uses the file name as the key

--- test/contextBuilder-tests.js
+++ test/contextBuilder-tests.js
@@ -3,4 +3,8 @@ var assert = require("assert");
 var contextBuilder = require("../lib/contextBuilder");
 
 describe("contextBuilder", function() {
+    it("object fields are the file names", function() {
+        var o = contextBuilder.build(__dirname + "/../sample/context/", ["index.json"]);
+        assert.notStrictEqual(undefined, o.index);
+    });
 });
