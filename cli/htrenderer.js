039d9d68983fc2116479ca5821ea6eefb885380c
Hendri Pretorius <pretorh@gmail.com>
2015-06-15 13:33:33 +0200

    add --root option to set root directory for other directories

--- cli/htrenderer.js
+++ cli/htrenderer.js
@@ -6,22 +6,26 @@ var args = getopt.create([
     ["c", "context=DIR" , "Context directory"],
     ["n", "nested=DIR"  , "Nested directory"],
     ["o", "output=DIR"  , "Output directory"],
+    ["r", "root=DIR"    , "Root directory.\n" +
+        "\tThe other 4 directories are assumed (unless specified explicitely) to be sub directories of this\n" +
+        "\tShort for: -t DIR/www/, -c DIR/context -n DIR/nested/ -o DIR/static/"],
 ])
 .bindHelp()
 .parseSystem();
 var values = args.options;
 
-function testValueMissing(field) {
+function testValueMissing(field, rootName) {
+    values[field] = values[field] || (values.root && values.root + "/" + rootName);
     if (!values[field]) {
         console.error("%s is required (--%s DIR)", field, field);
         process.exit(1);
     }
 }
 
-testValueMissing("template");
-testValueMissing("context");
-testValueMissing("nested");
-testValueMissing("output");
+testValueMissing("template", "www/");
+testValueMissing("context" , "context/");
+testValueMissing("nested"  , "nested/");
+testValueMissing("output"  , "static/");
 
 var options = {
     templateDir: values.template,
